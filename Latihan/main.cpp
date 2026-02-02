03b162bceb173a3491b6aac7f2f9dbc5669a6b77
ALChristos <jovan.cristo@gmail.com>
2026-02-02 20:24:05 +0800

    Menambahkan File program CPP

---
+++ Latihan/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //Include = copy and paste semua. iostream = agar bisa menampilkan program ke terminal
+/* coment all line */
+using namespace std;
+
+int main(){
+    cout<<"Apakah Berhasil???"<<endl; //cout = untuk menampilkan di terminal, endl = akhir dari line
+    return 0;
+}
