16263812ec1cc5d9908f7d0032990c20105a9231
ALChristos <jovan.cristo@gmail.com>
2026-02-02 22:33:20 +0800

    Menyelesaikan Tugas APDL 1

--- Tugas/APDL_S2_BuNovi/Tugas1.cpp
+++ Tugas/APDL_S2_BuNovi/Tugas1.cpp
@@ -1,7 +1,63 @@
+/*Nama Anggota Kelompok: (TUGAS APD LANJUT MEMBUAT ARRAY 1D)
+1. Muhammad Fakih Nabal (2509105009) 
+2. Muhammad Rafli Adila Risdi Saputra (2509106013) 
+3. Jovan Christo Alvaro (2509106031)
+*/
+
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout<<"Masukan Jumlah Data: ";
+    cin>>n;
+    system("cls");
 
+    vector<int> nilai(n);
+
+    for(int i = 0; i < n; i++){
+         cout<<"Masukan Nilai Data ke-"<<i + 1<<":";
+         cin>>nilai[i];
+         system("cls");
+    }
+
+    system("cls");
+
+    cout<<"Data Seluruh Nilai:";
+    for (int i = 0; i < n; i++){
+        cout<<" "<<nilai[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"\n";
+    for (int i = 0; i < n; i++){
+        cout<<"Nilai Ke-"<<i+1<<":"<<" "<<nilai[i]<<","<<" "<<"Grade:"<<" ";
+        
+        if(nilai[i] >= 80){
+            cout<<"A";
+        }
+        else if (nilai[i] >= 70){
+            cout<<"B";
+        }
+        else if (nilai[i] >= 60){
+            cout<<"C";
+        }
+        else if (nilai[i] >= 50){
+            cout<<"D";
+        }
+        else if (nilai[i] >= 40){
+            cout<<"E";
+        }
+        else if (nilai[i] > 100 && nilai[i] < 0 ){
+            cout<<"Nilai ERROR";
+        }
+        else{
+            cout<<"TIDAK LULUS";
+        }
+        cout<<endl;
+
+    }
+    return 0;
 }
